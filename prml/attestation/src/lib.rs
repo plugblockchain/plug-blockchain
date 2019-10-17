910559c7912ab9f1dc70f0dc4d3571a58027d53c
Jordan Beauchamp <beauchjord@gmail.com>
2019-10-18 10:24:46 +1300

    Add missing license header (#57)

--- prml/attestation/src/lib.rs
+++ prml/attestation/src/lib.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 //! # Attestation Module
 //!
 //! The Attestation module provides functionality for entities to create attestation claims about one another.
