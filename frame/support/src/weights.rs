f1ddead0ffdc41e3b9c7da3edbaa722ff189e965
Eric <e.lim0322@gmail.com>
2020-01-24 12:40:20 +1300

    Add a mock system::Trait and Origin for a test

--- frame/support/src/weights.rs
+++ frame/support/src/weights.rs
@@ -305,25 +305,28 @@ impl<AccountId: Encode, Call: Encode, Extra: Encode> GetDispatchInfo for sp_runt
 #[cfg(test)]
 #[allow(dead_code)]
 mod tests {
+	use crate::additional_traits::{DelegatedDispatchVerifier, MaybeDoughnutRef};
 	use crate::decl_module;
 	use super::*;
 
-	pub trait Trait {
-		type Origin;
-		type Balance;
-		type BlockNumber;
+	pub trait Trait: system::Trait + Sized where Self::AccountId: From<u32> {
+		type Origin: MaybeDoughnutRef<Doughnut=()>;
+		type BlockNumber: Into<u32>;
+		type Call: From<Call<Self>>;
 	}
 
-	pub struct TraitImpl {}
-
-	impl Trait for TraitImpl {
-		type Origin = u32;
-		type BlockNumber = u32;
-		type Balance = u32;
+	mod system {
+		use super::*;
+		pub trait Trait {
+			type AccountId;
+			type Balance;
+			type Doughnut;
+			type DelegatedDispatchVerifier: DelegatedDispatchVerifier<Doughnut = ()>;
+		}
 	}
 
 	decl_module! {
-		pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+		pub struct Module<T: Trait> for enum Call where origin: T::Origin, T::AccountId: From<u32> {
 			// no arguments, fixed weight
 			#[weight = SimpleDispatchInfo::FixedNormal(1000)]
 			fn f0(_origin) { unimplemented!(); }
@@ -335,6 +338,38 @@ mod tests {
 			#[weight = FunctionOf(|_: (&u32, &u32)| 0, DispatchClass::Operational, true)]
 			fn f12(_origin, _a: u32, _eb: u32) { unimplemented!(); }
 		}
+	}
+
+	pub struct TraitImpl {}
+
+	pub struct MockOrigin(pub u32);
+
+	impl MaybeDoughnutRef for MockOrigin {
+		type Doughnut = ();
+		fn doughnut(&self) -> Option<&Self::Doughnut> {
+			None
+		}
+	}
+
+	impl Trait for TraitImpl {
+		type Origin = MockOrigin;
+		type BlockNumber = u32;
+		type Call = OuterCall;
+	}
+
+	type Test = Module<TraitImpl>;
+
+	impl_outer_dispatch! {
+		pub enum OuterCall for TraitImpl where origin: MockOrigin {
+			self::Test,
+		}
+	}
+
+	impl system::Trait for TraitImpl {
+		type AccountId = u32;
+		type Balance = u32;
+		type Doughnut = ();
+		type DelegatedDispatchVerifier = ();
 	}
 
 	#[test]
