7bac4144e4f0d6d5f439fa1502f63dfc72731321
Hoani Bryson <hoani.bryson@gmail.com>
2020-01-29 14:15:52 +1300

    Generic Asset create requires root origin (#56)
    * Generic Asset create requires root origin
    
    * Update comments on generic asset create
    
    * Update changelog with generic asset interface change

--- frame/generic-asset/src/lib.rs
+++ frame/generic-asset/src/lib.rs
@@ -75,7 +75,8 @@
 //!
 //! ### Dispatchable Functions
 //!
-//! - `create`: Create a new kind of asset.
+//! - `create`: Create a new kind of asset and nominates the owner of this asset. The origin of this call must
+//! be root.
 //! - `transfer`: Transfer some liquid free balance to another account.
 //! - `update_permission`: Updates permission for a given `asset_id` and an account. The origin of this call
 //! must have update permissions.
@@ -358,24 +359,15 @@ decl_module! {
 
 		fn deposit_event() = default;
 
-		/// Create a new kind of asset.
-		fn create(origin, options: AssetOptions<T::Balance, T::AccountId>) -> DispatchResult {
-			let origin = ensure_signed(origin)?;
-			let id = Self::next_asset_id();
-
-			let permissions: PermissionVersions<T::AccountId> = options.permissions.clone().into();
-
-			// The last available id serves as the overflow mark and won't be used.
-			let next_id = id.checked_add(&One::one()).ok_or_else(|| Error::<T>::NoIdAvailable)?;
-
-			<NextAssetId<T>>::put(next_id);
-			<TotalIssuance<T>>::insert(id, &options.initial_issuance);
-			<FreeBalance<T>>::insert(&id, &origin, &options.initial_issuance);
-			<Permissions<T>>::insert(&id, permissions);
-
-			Self::deposit_event(RawEvent::Created(id, origin, options));
-
-			Ok(())
+		/// Create a new kind of asset and nominates the owner of this asset. The
+		/// origin of this call must be root.
+		fn create(
+			origin,
+			owner: T::AccountId,
+			options: AssetOptions<T::Balance, T::AccountId>
+		) -> DispatchResult {
+			ensure_root(origin)?;
+			Self::create_asset(None, Some(owner), options)
 		}
 
 		/// Transfer some liquid free balance to another account.
