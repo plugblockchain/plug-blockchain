4320ad4017365e6885bcb87c31819da34e0e9207
hoani <hoani.bryson@gmail.com>
2020-03-09 13:53:24 +1300

    Removing claims will only remove issuers if the issuer has no topics remaining

--- prml/attestation/src/lib.rs
+++ prml/attestation/src/lib.rs
@@ -72,9 +72,17 @@ decl_module! {
 		/// Remove a claim, only the original issuer can remove a claim
 		pub fn remove_claim(origin, holder: T::AccountId, topic: AttestationTopic) -> DispatchResult {
 			let issuer = ensure_signed(origin)?;
-			<Issuers<T>>::mutate(&holder,|issuers| issuers.retain(|vec_issuer| *vec_issuer != issuer));
-			<Topics<T>>::mutate((holder.clone(), issuer.clone()),|topics| topics.retain(|vec_topic| *vec_topic != topic));
 			<Values<T>>::remove((holder.clone(), issuer.clone(), topic));
+
+			<Topics<T>>::mutate((holder.clone(), issuer.clone()),|topics| topics.retain(|vec_topic| *vec_topic != topic));
+
+			<Issuers<T>>::mutate(&holder, |issuers| (
+					issuers.retain(|vec_issuer| (
+						*vec_issuer != issuer.clone() ||
+						Self::get_topics((holder.clone(), issuer.clone())).len() != 0
+					))
+			));
+
 
 			Self::deposit_event(RawEvent::ClaimRemoved(holder, issuer, topic));
 
@@ -259,6 +267,7 @@ mod tests {
 		let holder = 0xbaa;
 		let topic = AttestationTopic::from(0xf00d);
 		let value = AttestationValue::from(0xb33f);
+		let invalid_value = AttestationValue::zero();
 		ExtBuilder::build().execute_with(|| {
 			let result_add = Attestation::set_claim(Origin::signed(issuer), holder, topic, value);
 
@@ -269,7 +278,88 @@ mod tests {
 
 			assert_eq!(Attestation::get_issuers(holder), []);
 			assert_eq!(Attestation::get_topics((holder, issuer)), []);
-			assert_eq!(Attestation::get_value((holder, issuer, topic)), AttestationValue::from(0));
+			assert_eq!(Attestation::get_value((holder, issuer, topic)), invalid_value);
+		})
+	}
+
+	#[test]
+	fn remove_claim_from_account_with_multiple_issuers() {
+		let issuer_foo = 0xf00;
+		let issuer_boa = 0xb0a;
+		let holder = 0xbaa;
+		let topic_food = AttestationTopic::from(0xf00d);
+		let value_food_foo = AttestationValue::from(0xb33f);
+		let value_food_boa = AttestationValue::from(0x90a7);
+		let invalid_value = AttestationValue::zero();
+		ExtBuilder::build().execute_with(|| {
+			let result_foo = Attestation::set_claim(Origin::signed(issuer_foo), holder, topic_food, value_food_foo);
+			let result_boa = Attestation::set_claim(Origin::signed(issuer_boa), holder, topic_food, value_food_boa);
+
+			let result_remove = Attestation::remove_claim(Origin::signed(issuer_foo), holder, topic_food);
+
+			assert_eq!(result_foo, Ok(()));
+			assert_eq!(result_boa, Ok(()));
+			assert_eq!(result_remove, Ok(()));
+
+			assert_eq!(Attestation::get_issuers(holder), [issuer_boa]);
+			assert_eq!(Attestation::get_topics((holder, issuer_foo)), []);
+			assert_eq!(Attestation::get_topics((holder, issuer_boa)), [topic_food]);
+			assert_eq!(Attestation::get_value((holder, issuer_foo, topic_food)), invalid_value);
+			assert_eq!(Attestation::get_value((holder, issuer_boa, topic_food)), value_food_boa);
+		})
+	}
+
+	#[test]
+	fn remove_claim_from_account_with_multiple_claims_from_same_issuer() {
+		let issuer = 0xf00;
+		let holder = 0xbaa;
+		let topic_food = AttestationTopic::from(0xf00d);
+		let value_food = AttestationValue::from(0xb33f);
+		let topic_loot = AttestationTopic::from(0x1007);
+		let value_loot = AttestationValue::from(0x901d);
+		let invalid_value = AttestationValue::zero();
+		ExtBuilder::build().execute_with(|| {
+			let result_food = Attestation::set_claim(Origin::signed(issuer), holder, topic_food, value_food);
+			let result_loot = Attestation::set_claim(Origin::signed(issuer), holder, topic_loot, value_loot);
+
+			let result_remove = Attestation::remove_claim(Origin::signed(issuer), holder, topic_food);
+
+			assert_eq!(result_food, Ok(()));
+			assert_eq!(result_loot, Ok(()));
+			assert_eq!(result_remove, Ok(()));
+
+			assert_eq!(Attestation::get_issuers(holder), [issuer]);
+			assert_eq!(Attestation::get_topics((holder, issuer)), [topic_loot]);
+			assert_eq!(Attestation::get_value((holder, issuer, topic_food)), invalid_value);
+			assert_eq!(Attestation::get_value((holder, issuer, topic_loot)), value_loot);
+		})
+	}
+
+	#[test]
+	fn issuer_is_removed_if_there_are_no_claims_left() {
+		let issuer = 0xf00;
+		let holder = 0xbaa;
+		let topic_food = AttestationTopic::from(0xf00d);
+		let value_food = AttestationValue::from(0xb33f);
+		let topic_loot = AttestationTopic::from(0x1007);
+		let value_loot = AttestationValue::from(0x901d);
+		let invalid_value = AttestationValue::zero();
+		ExtBuilder::build().execute_with(|| {
+			let result_food = Attestation::set_claim(Origin::signed(issuer), holder, topic_food, value_food);
+			let result_loot = Attestation::set_claim(Origin::signed(issuer), holder, topic_loot, value_loot);
+
+			let result_remove_food = Attestation::remove_claim(Origin::signed(issuer), holder, topic_food);
+			let result_remove_loot = Attestation::remove_claim(Origin::signed(issuer), holder, topic_loot);
+
+			assert_eq!(result_food, Ok(()));
+			assert_eq!(result_loot, Ok(()));
+			assert_eq!(result_remove_food, Ok(()));
+			assert_eq!(result_remove_loot, Ok(()));
+
+			assert_eq!(Attestation::get_issuers(holder), []);
+			assert_eq!(Attestation::get_topics((holder, issuer)), []);
+			assert_eq!(Attestation::get_value((holder, issuer, topic_food)), invalid_value);
+			assert_eq!(Attestation::get_value((holder, issuer, topic_loot)), invalid_value);
 		})
 	}
 
