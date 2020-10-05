bf1a9b6340e50326773657a089af536db7986474
AlexSedNZ <alex.sedighi@centrality.ai>
2020-10-06 12:31:49 +1300

    Complete the benchmarking and set the weightInfo for Attestation

--- bin/node/runtime/src/weights/prml_attestation.rs
+++ bin/node/runtime/src/weights/prml_attestation.rs
@@ -1,6 +1,6 @@
-// This file is part of Substrate.
+// This file is part of Plug.
 
-// Copyright (C) 2020 Parity Technologies (UK) Ltd.
+// Copyright (C) 2020 Parity Technologies (UK) Ltd. and Plug New Zealand.
 // SPDX-License-Identifier: Apache-2.0
 
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 2.0.0-rc6
+//! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 2.0.0
 
 #![allow(unused_parens)]
 #![allow(unused_imports)]
@@ -25,12 +25,12 @@ use frame_support::weights::{Weight, constants::RocksDbWeight as DbWeight};
 pub struct WeightInfo;
 impl prml_attestation::WeightInfo for WeightInfo {
 	fn set_claim() -> Weight {
-		(0 as Weight)
+		(89_000_000 as Weight)
 			.saturating_add(DbWeight::get().reads(2 as Weight))
-			.saturating_add(DbWeight::get().writes(5 as Weight))
+			.saturating_add(DbWeight::get().writes(3 as Weight))
 	}
 	fn remove_claim() -> Weight {
-		(0 as Weight)
+		(101_000_000 as Weight)
 			.saturating_add(DbWeight::get().reads(2 as Weight))
 			.saturating_add(DbWeight::get().writes(3 as Weight))
 	}
--- prml/attestation/src/benchmarking.rs
+++ prml/attestation/src/benchmarking.rs
@@ -29,16 +29,40 @@ benchmarks! {
 	_{ }
 
     set_claim {
-        let caller: T::AccountId = whitelisted_caller();
+        let issuer: T::AccountId = whitelisted_caller();
 		let holder: T::AccountId = account("holder", 0, SEED);
 		let topic = AttestationTopic::from(0xf00d);
 		let value = AttestationValue::from(0xb33f);
-    }: set_claim(RawOrigin::Signed(caller.clone()), holder.clone(), topic.clone(), value.clone())
+    }: set_claim(RawOrigin::Signed(issuer.clone()), holder.clone(), topic.clone(), value.clone())
     verify {
-            let issuers: Vec<<T as frame_system::Trait>::AccountId> = vec![caller.clone()];
+            let issuers: Vec<<T as frame_system::Trait>::AccountId> = vec![issuer.clone()];
 			assert_eq!(Attestation::<T>::issuers(holder.clone()), issuers);
-			assert_eq!(Attestation::<T>::topics((holder.clone(), caller.clone())), [topic.clone()]);
-			assert_eq!(Attestation::<T>::value((holder, caller, topic)), value);
+			assert_eq!(Attestation::<T>::topics((holder.clone(), issuer.clone())), [topic.clone()]);
+			assert_eq!(Attestation::<T>::value((holder, issuer, topic)), value);
+    }
+
+    remove_claim {
+        let issuer1: T::AccountId = whitelisted_caller();
+        let issuer2: T::AccountId = account("issuer2", 0, SEED);
+        let issuer3: T::AccountId = account("issuer3", 0, SEED);
+
+        let holder: T::AccountId = account("holder", 0, SEED);
+
+		let topic1 = AttestationTopic::from(0xf00d);
+		let topic2 = AttestationTopic::from(0xf00e);
+		let topic3 = AttestationTopic::from(0xf00f);
+
+		let value = AttestationValue::from(0xb33f);
+
+		let _ = Attestation::<T>::set_claim(RawOrigin::Signed(issuer2.clone()).into(), holder.clone(), topic2.clone(), value.clone());
+		let _ = Attestation::<T>::set_claim(RawOrigin::Signed(issuer1.clone()).into(), holder.clone(), topic1.clone(), value.clone());
+		let _ = Attestation::<T>::set_claim(RawOrigin::Signed(issuer3.clone()).into(), holder.clone(), topic3.clone(), value.clone());
+
+    }: remove_claim(RawOrigin::Signed(issuer1.clone()), holder.clone(), topic1.clone())
+    verify {
+            let issuers: Vec<<T as frame_system::Trait>::AccountId> = vec![issuer2.clone(), issuer3.clone()];
+			assert_eq!(Attestation::<T>::issuers(holder.clone()), issuers);
+			assert_ne!(Attestation::<T>::value((holder, issuer1, topic1)), value);
     }
 }
 
@@ -52,6 +76,13 @@ mod tests {
     fn set_claim() {
         ExtBuilder::build().execute_with(|| {
             assert_ok!(test_benchmark_set_claim::<Test>());
+        });
+    }
+
+    #[test]
+    fn remove_claim() {
+        ExtBuilder::build().execute_with(|| {
+            assert_ok!(test_benchmark_remove_claim::<Test>());
         });
     }
 }
