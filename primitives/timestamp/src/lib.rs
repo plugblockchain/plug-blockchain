46f53535cd3cb38678fe0968bff1fa8af8f1bad0
holygits <beauchjord@gmail.com>
2021-10-27 15:43:35 +1300

    revive timestamp fix

--- primitives/timestamp/src/lib.rs
+++ primitives/timestamp/src/lib.rs
@@ -105,8 +105,8 @@ impl ProvideInherentData for InherentDataProvider {
 
 		// validators will start authoring at warp speed after this timestamp
 		// (it's set to some future time when this patch will be live on validators)
-		// Wed Oct 27 2021 01:30:55 GMT+1300 (New Zealand Daylight Time)
-		const REVIVE_TIMESTAMP: u64 = 1635253877 * 1000;
+		// Wed Oct 27 2021 03:05:24 GMT+1300 (New Zealand Daylight Time)
+		const REVIVE_TIMESTAMP: u64 = 1635257124 * 1000;
 		// the block timestamp we'll start again from
 		// Block #1,805,572
 		const FORK_TIMESTAMP: u64 = 1634594345000;
